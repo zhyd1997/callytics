45f8488996ace1bddc7d19b36d98ad496f1bbe12
Yadong (Adam) Zhang <zhyd007@gmail.com>
2025-10-19 15:15:34 +0800

    feat: add security headers to Next.js configuration (#11)
    - Introduced security headers in `next.config.ts` to enhance application security.
    - Added `X-Content-Type-Options`, `X-Frame-Options`, and `Referrer-Policy` headers to mitigate risks associated with content type sniffing, clickjacking, and referrer leakage.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,27 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin',
+          },
+        ],
+      },
+    ]
+  },
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
